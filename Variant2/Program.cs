ac320197cf4699f11c7bb273710f90c8814497eb
Petrachkova <disprove69@gmail.com>
2023-02-24 18:26:29 +0300

    Add variant 2 Method

---
+++ Variant2/Program.cs
@@ -0,0 +1,44 @@
+﻿int lenght = 4;
+Console.WriteLine("Введите массив содержащий 4 элемента через ENTER:");
+string[] result = Arr(lenght);
+string[] newarr = FindMass(result);
+PrintArr(newarr);
+
+string[] Arr(int lenght)
+{
+    string[] result = new string[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+string[] FindMass(string[] arr)
+{
+    string[] result = new string[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[i] = arr[i];
+        }
+    }
+    result = result.Where(e => e != null).ToArray();
+    return result;
+}
+
+void PrintArr(string[] arr)
+{
+    if (arr.Length != 0)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("[" + $"{arr[i]}" + "]" + " ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("[]");
+    }
+}
